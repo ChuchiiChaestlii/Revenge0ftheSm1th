0e3dae03163c47c5330a5091fbb6bce8cfce375a
ChuchiiChaestlii <kevin.willimann@hispeed.ch>
2018-11-03 02:06:34 +0100

    Initial Commit

---
+++ js/main.js
@@ -0,0 +1,15 @@
+var options = {
+    options: {
+        debug: false
+    },
+    connection: {
+        cluster: "aws",
+        reconnect: true
+    },
+    identity: {
+        username: "chuchi_bot",
+        password: "oauth:wgt8wn2il4ezvvmno47fkqu6yypidt"
+    },
+    channels: ['revenge0fthesm1th']
+};
+
